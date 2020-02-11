cedb035d0d72f3b872c58ee62c3125d7451e6d73
DeveloperOfEverything <50147071+DeveloperOfEverything@users.noreply.github.com>
2020-02-11 15:22:20 +0500

    Add files via upload

---
+++ IprDownloader.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         IPR Books Downloader
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Данный скрипт позволяет скачивать литературу из библиотеки IPR Books в формате PDF. Для этого необходимо перейти в режим чтения книги и нажать соответствующую кнопку на панели инструментов.
+// @author       DeveloperOfEverything
+// @match        http://www.iprbookshop.ru/epd-reader*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var toolBar = document.getElementById('toolbarViewerRight');
+
+    var saveButton = document.createElement('input');
+
+    var neighborButton = document.createElement('viewBookmark');
+
+    saveButton.type = "Button";
+    saveButton.value = "Скачать";
+    saveButton.style.height = 35 + "px";
+    saveButton.style.marginTop = 6 + "px";
+
+    saveButton.onclick = function()
+    {
+        PDFViewerApplication.download();
+    };
+
+    toolBar.appendChild(saveButton);
+
+})();
