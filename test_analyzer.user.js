6253c332d49b825ea455a446716b30d4c09a6817
DeveloperOfEverything <50147071+DeveloperOfEverything@users.noreply.github.com>
2020-02-25 02:03:01 +0500

    Update test_analyzer.user.js

--- test_analyzer.user.js
+++ test_analyzer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TestAnalyzer
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  Скрипт анализа тестов.
 // @author       URE_Community
 // @match        docs.google.com/forms*
@@ -20,6 +20,7 @@
     var sidePanelWidth = "40px";
     var buttonLeft = "10px";
     var buttonWidth = "20px";
+    var fullWidth = "150px";
 
     var sidePanelBottom = "50px";
 
@@ -52,32 +53,44 @@
 
             //=====[ Кнопка скрыть/показать ]=====
             var showButton = document.createElement('div');
+            var hideButton = document.createElement('div');
 
-            function Show()
+            function ShowFull()
             {
+                toolsContainer.hidden = false;
+
                 UIBase.style.width = null;
                 UIBase.style.left = "12px";
                 UIBase.style.bottom = "12px";
 
-                showButton.innerText = ">>";
+                hideButton.onclick = Show;
+            }
 
-                showButton.onclick = Hide;
+            function Show()
+            {
+                toolsContainer.hidden = false;
 
-                console.log(getQuestions());
+                UIBase.style.width = fullWidth;//null;
+                UIBase.style.left = null;
+                UIBase.style.bottom = "12px";
+
+                showButton.onclick = ShowFull;
+                hideButton.onclick = Hide;
             }
 
             function Hide()
             {
+                toolsContainer.hidden = true;
+
                 UIBase.style.width = sidePanelWidth;
                 UIBase.style.left = null;
                 UIBase.style.bottom = sidePanelBottom;
-
-                showButton.innerText = "<<";
 
                 showButton.onclick = Show;
             }
 
             showButton.onclick = Show;
+            hideButton.onclick = Hide;
 
             showButton.style.width = buttonWidth;
             showButton.style.height = "60px";
@@ -93,23 +106,230 @@
 
             showButton.innerText = "<<";
 
+            hideButton.style.width = buttonWidth;
+            hideButton.style.height = "60px";
+            hideButton.style.left = buttonLeft;
+            hideButton.style.top = "50%";
+            hideButton.style.position = "absolute";
+            hideButton.style.backgroundColor = "#673ab7";
+            hideButton.style.color = "#FFF";
+            hideButton.style.borderRadius = "4px";
+            hideButton.style.lineHeight="58px";
+            hideButton.style.textAlign="center";
+            hideButton.style.cursor="pointer";
+
+            hideButton.innerText = ">>";
+
             UIBase.append(showButton);
+
+            UIBase.append(hideButton);
 
             //=====[ Контейнер пользовательских инструментов ]=====
             var toolsContainer = document.createElement('div');
 
             toolsContainer.style.position = "absolute";
+            //toolsContainer.style.padding = "20px";
             toolsContainer.style.left = sidePanelWidth;
-            toolsContainer.style.right = sidePanelWidth;
+            toolsContainer.style.right = "0px";//sidePanelWidth;
             toolsContainer.style.top = sidePanelWidth;
             toolsContainer.style.bottom = sidePanelWidth;
             toolsContainer.style.backgroundColor = "#f1ecec";
-            toolsContainer.style.overflow = "auto";
+            //toolsContainer.style.overflow = "auto";
+
+            //=====[ Элементы интерфейса ]=====
+            var help = document.createElement('div');
+
+            help.id = "help";
+            help.innerText = "Перетащите файл ключей";
+
+            help.style.textAlign = "center";
+            help.style.marginTop = "10px";
+            help.style.marginBottom = "10px";
+
+            var getPatternBtn = document.createElement('div');
+
+            getPatternBtn.className = "appsMaterialWizButtonEl appsMaterialWizButtonPaperbuttonEl appsMaterialWizButtonPaperbuttonFilled freebirdFormeditorViewHeaderHeaderMenu freebirdFormeditorViewHeaderSendButton isUndragged";
+            getPatternBtn.style.paddingLeft = "10px";
+            getPatternBtn.style.paddingRight = "10px";
+            getPatternBtn.style.marginTop = "10px";
+            getPatternBtn.style.marginBottom = "10px";
+            getPatternBtn.innerText = "Получить шаблон";
+            getPatternBtn.onclick = savePattern;
+
+            toolsContainer.append(getPatternBtn);
+            toolsContainer.append(help);
+
+            var resContainer = document.createElement('div');
+
+            resContainer.style.width = "100%";
+
+            toolsContainer.append(resContainer);
+
+            //=====[ Функции анализа ]=====
+
+            var checkQuestion = function(u, l)
+            {
+                var ans = u.answer;
+
+                //Обработка действий default
+                for (var i=0; i<l.default.length; i++)
+                {
+                    var v = l.default[i].variable;
+                    eval("globalThis.testActionHandler = function() { return " + l.default[i].action + ";}")
+
+                    if (res[v] == undefined) res[v] = testActionHandler();
+                    else res[v] += testActionHandler();
+                }
+
+                for (var j=0; j<l.answers.length; j++)
+                {
+                    if (l.answers[j].answer == ans)
+                    {
+                        for (var k=0; k<l.answers[j].actions.length; k++)
+                        {
+                            var va = l.answers[j].actions[k].variable;
+                            eval("globalThis.testActionHandler = function() { return " + l.answers[j].actions[k].action + ";}");
+
+                            if (res[va] == undefined) res[va] = testActionHandler();
+                            else res[va] += testActionHandler();
+                        }
+                    }
+                }
+            }
+
+            var ta;
+            var sb;
+
+            var start = function()
+            {
+                if (ta != undefined)
+                    resContainer.removeChild(ta);
+
+                if (sb != undefined)
+                    resContainer.removeChild(sb);
+
+                var q_u = getQuestions();
+                var q_l = rules.questions;
+
+                if (q_u.length != q_l.length)
+                {
+                    alert("Число вопросов в тесте ("+q_u.length+") не совпадает с числом в файле ключей ("+q_l.length+").");
+                    return;
+                }
+
+                globalThis.res = new Object();
+
+                for (var i=0; i<q_u.length; i++)
+                {
+                    checkQuestion(q_u[i], q_l[i]);
+                }
+
+                var textArea = document.createElement('textarea');
+
+                ta = textArea;
+
+                textArea.style.margin = "10px";
+                textArea.style.width = "-webkit-fill-available";
+                textArea.style.resize = "vertical";
+
+                resContainer.append(textArea);
+
+                textArea.value = "Результат\r\n";
+
+                for (var keys in res)
+                {
+                    textArea.value += keys + "\t" + res[keys] + "\r\n";
+                }
+
+                textArea.value += "\r\nОтветы\r\n";
+
+                for (var j=0; j<q_u.length; j++)
+                {
+                    textArea.value += (j+1) + "\t" + q_u[j].question + "\t" + q_u[j].answer + "\r\n";
+                }
+
+                var saveBtn = document.createElement('div');
+
+                sb = saveBtn;
+
+                saveBtn.className = "appsMaterialWizButtonEl appsMaterialWizButtonPaperbuttonEl appsMaterialWizButtonPaperbuttonFilled freebirdFormeditorViewHeaderHeaderMenu freebirdFormeditorViewHeaderSendButton isUndragged";
+                saveBtn.style.paddingLeft = "10px";
+                saveBtn.style.paddingRight = "10px";
+                saveBtn.style.marginTop = "10px";
+                saveBtn.style.marginBottom = "10px";
+                saveBtn.innerText = "Сохранить результат";
+                saveBtn.onclick = function()
+                {
+                    var dataLine = "";
+
+                    for (var keys in res)
+                    {
+                        dataLine += keys + ";" + res[keys] + "\r\n";
+                    }
+
+                    dataLine += "\r\n";
+
+                    for (var j=0; j<q_u.length; j++)
+                    {
+                        dataLine += (j+1) + ";" + q_u[j].question + ";" + q_u[j].answer + "\r\n";
+                    }
+
+                    saveWinData("result.csv",dataLine);
+                }
+
+                resContainer.append(saveBtn);
+            }
+
+            var loadTestRules = function(test)
+            {
+                resContainer.removeChildren(resContainer.children);
+
+                var testName = document.createElement('div');
+
+                resContainer.append(testName);
+
+                globalThis.rules = JSON.parse(test);
+
+                testName.innerText = rules.name;
+                testName.style.textAlign = "center";
+                testName.style.marginTop = "10px";
+                testName.style.marginBottom = "10px";
+
+                var startBtn = document.createElement('div');
+
+                startBtn.className = "appsMaterialWizButtonEl appsMaterialWizButtonPaperbuttonEl appsMaterialWizButtonPaperbuttonFilled freebirdFormeditorViewHeaderHeaderMenu freebirdFormeditorViewHeaderSendButton isUndragged";
+                startBtn.style.paddingLeft = "10px";
+                startBtn.style.paddingRight = "10px";
+                startBtn.style.marginTop = "10px";
+                startBtn.style.marginBottom = "10px";
+                startBtn.innerText = "Запустить анализ";
+                startBtn.onclick = start;
+
+                resContainer.append(startBtn);
+            }
+
+            var loadFile = function(file)
+            {
+                file.text().then(loadTestRules);
+            }
+
+            toolsContainer.addEventListener("dragover", function(event) {
+                event.preventDefault(); // отменяем действие по умолчанию
+            }, false);
+            toolsContainer.addEventListener("drop", function(event) {
+                // отменяем действие по умолчанию
+                event.preventDefault();
+                var files = event.dataTransfer.files;
+
+                loadFile(files[0]);
+            }, false);
 
             UIBase.append(toolsContainer);
 
-            //=====[ Функции анализа ]=====
-            var baseElement = document.getElementsByClassName('freebirdFormeditorViewResponsesRecordviewResponseContainer')[0];
+            var clearToolbox = function()
+            {
+                resContainer.removeChildren(resContainer.children);
+            }
         }
     }
 
@@ -128,7 +348,7 @@
 
     //=====[ Формируем список вопросов ]=====
 
-    function getQuestions()
+    globalThis.getQuestions = function()
     {
         var qBlocks = document.getElementsByClassName('freebirdFormviewerViewItemsItemItem');
 
@@ -136,30 +356,167 @@
 
         for (var i=0; i<qBlocks.length; i++)
         {
-            questions[questions.length] = new Object();
-            questions[questions.length-1].q = qBlocks[i].children[0].children[0].innerText;
-
-            var questionType = qBlocks[i].children[1].class;
-
-            switch (questionType)
+            if (qBlocks[i].offsetHeight != 0 || qBlocks[i].offsetWidth != 0)
             {
-                case "freebirdFormviewerViewItemsTextShortText freebirdFormviewerViewItemsTextDisabledText freebirdThemedInput": //Открытый вопрос
-                    {
+                questions[questions.length] = new Object();
 
-                        break;
-                    }
-                case "appsMaterialWizToggleRadiogroupGroupContainer exportGroupContainer freebirdFormviewerViewItemsRadiogroupRadioGroup": //Вопрос с вариантом ответа
-                    {
+                var question = qBlocks[i].children[0].children[0].innerText;
 
-                        break;
-                    }
+                if (question.endsWith(' *')) question = question.substr(0,question.length-2)
+
+                questions[questions.length-1].question = question;
+
+                var ansContainer = qBlocks[i].children[1].children[0]
+                var questionType = ansContainer.className;
+
+                switch (questionType)
+                {
+                    case "freebirdFormviewerViewItemsTextShortText freebirdFormviewerViewItemsTextDisabledText freebirdThemedInput": //Открытый вопрос
+                        {
+                            questions[questions.length-1].answer = ansContainer.innerText;
+                            break;
+                        }
+                    case "appsMaterialWizToggleRadiogroupGroupContainer exportGroupContainer freebirdFormviewerViewItemsRadiogroupRadioGroup": //Вопрос с вариантом ответа
+                        {
+                            questions[questions.length-1].answer = ansContainer.attributes['data-value'].value;
+                            break;
+                        }
+                }
             }
         }
 
         return questions;
     }
 
+    //=====[ Собираем шаблон ключей ]=====
+
+    globalThis.getPattern = function()
+    {
+        var test = new Object();
+        test.name = "";
+
+        var qBlocks = document.getElementsByClassName('freebirdFormviewerViewItemsItemItem');
+
+        var questions = [];
+
+        for (var i=0; i<qBlocks.length; i++)
+        {
+            if (qBlocks[i].offsetHeight != 0 || qBlocks[i].offsetWidth != 0)
+            {
+                var question = qBlocks[i].children[0].children[0].innerText;
+
+                var q = new Object();
+
+                if (question.endsWith(' *')) question = question.substr(0,question.length-2)
+
+                q.question = question;
+                q.answers = [];
+
+                var ansContainer = qBlocks[i].children[1].children[0]
+                var questionType = ansContainer.className;
+
+                if (questionType == "appsMaterialWizToggleRadiogroupGroupContainer exportGroupContainer freebirdFormviewerViewItemsRadiogroupRadioGroup") //Вопрос с вариантом ответа
+                {
+                    var answers = ansContainer.getElementsByClassName('freebirdFormviewerViewItemsRadioOptionContainer');
+
+                    for (var j=0; j<answers.length; j++)
+                    {
+                        q.answers[q.answers.length] = new Object();
+                        q.answers[q.answers.length-1].answer = answers[j].innerText//ansContainer.attributes['data-value'].value;
+                    }
+                }
+
+                questions[questions.length] = q;
+            }
+        }
+
+        test.questions = questions;
+
+        return test;
+    }
+
+    globalThis.decodeCP1251 = function(string)
+    {
+        function decodeChar(s, p)
+        {
+            var cp1251 = 'ЂЃ‚ѓ„…†‡€‰Љ‹ЊЌЋЏђ‘’“”•–—�™љ›њќћџ ЎўЈ¤Ґ¦§Ё©Є«¬*®Ї°±Ііґµ¶·\
+ё№є»јЅѕїАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюя';//Эта переменная отображается тут неверна, см. ее ниже!
+            p = parseInt(p, 16);
+            return p < 128 ? String.fromCharCode(p) : cp1251[p - 128];
+        }
+        var str = string;
+        return str.replace(/%(..)/g,decodeChar);
+    }
+
+    globalThis.encodeCP1251 = function (string)
+    {
+        function encodeChar(c)
+        {
+            var isKyr = function (str)
+            {
+                return /[а-я]/i.test(str);
+            }
+            var cp1251 = 'ЂЃ‚ѓ„…†‡€‰Љ‹ЊЌЋЏђ‘’“”•–—�™љ›њќћџ ЎўЈ¤Ґ¦§Ё©Є«¬*®Ї°±Ііґµ¶·\
+ё№є»јЅѕїАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюя';//Эта переменная записана неверно, см. ее ниже!
+
+            var p = isKyr(c) ? (cp1251.indexOf(c) + 128) : c.charCodeAt(0);
+            var h = p.toString(16);
+            return '%' + h;
+        }
+
+        var res = '';
+        for (var i = 0; i < string.length; i++)
+        {
+            res += encodeChar(string.charAt(i)) //ну или string[i]
+        }
+        return res;
+    }
+
+    globalThis.saveWinData = function(fileName, string)
+    {
+        var csvString = encodeCP1251(string);
+        while (csvString.indexOf('%d%a') > -1)
+            csvString = csvString.replace('%d%a','%0D%0A');
+        while (csvString.indexOf('%401') > -1)
+            csvString = csvString.replace('%401','%A8');
+        while (csvString.indexOf('%451') > -1)
+            csvString = csvString.replace('%451','%B8');
+        var a = document.createElement('a');
+        a.href = 'data:attachment/csv,' + csvString
+        a.target = '_blank';
+        a.download = fileName;
+
+        document.body.appendChild(a);
+        a.click();
+    }
+
+    globalThis.saveData = function(fileName, string)
+    {
+        var csv = string;
+        var csvData = 'data:application/json;charset=utf-8,' + encodeURIComponent(csv);
+        var a = document.createElement('a');
+        a.href = csvData;
+        a.target = '_blank';
+        a.download = fileName;
+        a.click();
+    }
+
+    globalThis.savePattern = function()
+    {
+        var pattern = getPattern();
+        var patternLine = JSON.stringify(pattern);
+        globalThis.saveData("pattern.json", patternLine);
+    }
+
     //=====[ Вспомогательные функции ]=====
+
+    function stringToFunction(string)
+    {
+        eval("function tempFunction(){ return" + string + ";}");
+
+        return tempFunction;
+    }
+
     var getAllElements = function(parentElement = document.body)
     {
         var temp = [];
@@ -178,6 +535,12 @@
     HTMLElement.prototype.getAllElements = function()
     {
         return getAllElements(this);
+    }
+
+    HTMLElement.prototype.removeChildren = function(children)
+    {
+        while(children.length > 0)
+            this.removeChild(children[0]);
     }
 
     HTMLElement.prototype.getElementsByAttributeValue = function(attribute, value)
