f62e8b08502fafddc9da7a914c6f320ecc50907f
DeveloperOfEverything <50147071+DeveloperOfEverything@users.noreply.github.com>
2020-04-16 03:13:51 +0500

    Update extension_manager.user.js

--- extension_manager.user.js
+++ extension_manager.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ExtensionManager
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  ExtensionManager for downloading JS scripts
 // @author       DeveloperOfEverything
 // @match        https://ya.ru
