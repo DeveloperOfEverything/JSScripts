3b8f7445194e3d516ece5832bf49eff7fc929f6d
DeveloperOfEverything <50147071+DeveloperOfEverything@users.noreply.github.com>
2020-09-29 20:39:29 +0500

    Create wotspeak_redirect.user.js

---
+++ wotspeak_redirect.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         WotSpeakRedirect
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://wotspeak.org/engine/dwn_just2.php*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var scripts = document.getElementsByTagName('script');
+    var i=0;
+
+    while (!scripts[i].innerText.startsWith("\n(function() {\n	var i = 10;//время в секундах.")) i++;
+
+    var script = scripts[i].innerText;
+
+    var start = script.indexOf("href=\"")+6;
+    var end = script.indexOf("\" class=\"button7\"");
+
+    var src = script.substring(start, end);
+
+    document.location.href = src;
+
+    // Your code here...
+})();
