387341c3d0efcf130e01c18aff608c10372028a2
DeveloperOfEverything <50147071+DeveloperOfEverything@users.noreply.github.com>
2022-05-20 21:17:44 +0500

    Update no_yandex_ad.user.js

--- no_yandex_ad.user.js
+++ no_yandex_ad.user.js
@@ -7,6 +7,8 @@
 // @match        https://yandex.ru/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=yandex.ru
 // @grant        none
+// @updateUrl	https://raw.githubusercontent.com/DeveloperOfEverything/JSScripts/master/show_passwords.user.js
+// @downloadUrl	https://raw.githubusercontent.com/DeveloperOfEverything/JSScripts/master/show_passwords.user.js
 // ==/UserScript==
 
 (function() {
