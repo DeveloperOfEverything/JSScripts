78e42faa4361cb64bfa7cc0856239298e60553d3
DeveloperOfEverything <50147071+DeveloperOfEverything@users.noreply.github.com>
2022-06-05 23:02:37 +0500

    Update wotspeak_redirect.user.js

--- wotspeak_redirect.user.js
+++ wotspeak_redirect.user.js
@@ -4,7 +4,7 @@
 // @version      0.2
 // @description  try to take over the world!
 // @author       You
-// @match        https://wotspeak.org/engine/dwn_just2.php*
+// @match        https://wotspeak.org/engine/dwn_just2-car.php?xf=*
 // @match        https://ya.ru/
 // @match        https://vk.com/feed
 // @grant        none
