b433ec55dcd5ecc04116c2f5d67ec101fb2f171a
DeveloperOfEverything <50147071+DeveloperOfEverything@users.noreply.github.com>
2022-06-05 23:03:26 +0500

    Update wotspeak_redirect.user.js

--- wotspeak_redirect.user.js
+++ wotspeak_redirect.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WotSpeakRedirect
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://wotspeak.org/engine/dwn_just2-car.php?xf=*
