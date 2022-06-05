2e2fe6c367cd0d67d501706dc5ddfb0909f68759
DeveloperOfEverything <50147071+DeveloperOfEverything@users.noreply.github.com>
2022-06-05 22:59:44 +0500

    Update wotspeak_redirect.user.js

--- wotspeak_redirect.user.js
+++ wotspeak_redirect.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         WotSpeakRedirect
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://wotspeak.org/engine/dwn_just2.php*
+// @match        https://ya.ru/
+// @match        https://vk.com/feed
 // @grant        none
 // @updateUrl	https://raw.githubusercontent.com/DeveloperOfEverything/JSScripts/master/wotspeak_redirect.user.js
 // @downloadUrl	https://raw.githubusercontent.com/DeveloperOfEverything/JSScripts/master/wotspeak_redirect.user.js
@@ -14,21 +16,12 @@
     'use strict';
     
     if (globalThis.myExtensions == null) globalThis.myExtensions = [];
-    globalThis.myExtensions[globalThis.myExtensions.length] = "https://raw.githubusercontent.com/DeveloperOfEverything/JSScripts/master/wotspeak_redirect.user.js";
-
-    var scripts = document.getElementsByTagName('script');
-    var i=0;
-
-    while (!scripts[i].innerText.startsWith("\n(function() {\n	var i = 10;//время в секундах.")) i++;
-
-    var script = scripts[i].innerText;
-
-    var start = script.indexOf("href=\"")+6;
-    var end = script.indexOf("\" class=\"button7\"");
-
-    var src = script.substring(start, end);
-
-    document.location.href = src;
-
+    globalThis.myExtensions[globalThis.myExtensions.length] = "https://raw.githubusercontent.com/DeveloperOfEverything/JSScripts/master/ipr_downloader.user.js";
+    
+    if (document.URL.startsWith("https://vk.com") || document.URL =="https://ya.ru/") return;
     // Your code here...
+
+    var text = document.body.getElementsByTagName('script')[16].innerText;
+    text = text.substr(300, text.length-557);
+    window.location.href = text;
 })();
