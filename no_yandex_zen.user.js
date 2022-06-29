7d3fbef237f2108ae71a36bce326034524f88436
DeveloperOfEverything <50147071+DeveloperOfEverything@users.noreply.github.com>
2022-06-29 10:15:53 +0500

    Update no_yandex_zen.user.js

--- no_yandex_zen.user.js
+++ no_yandex_zen.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         NoYandexDzen
+// @name         NoYandexZen
 // @namespace    http://tampermonkey.net/
 // @version      0.2
 // @description  try to take over the world!
