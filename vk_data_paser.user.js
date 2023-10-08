c105026a848b641170951b85e43a83293bda9f02
DeveloperOfEverything <50147071+DeveloperOfEverything@users.noreply.github.com>
2023-10-08 19:57:36 +0500

    Update vk_data_paser.user.js

--- vk_data_paser.user.js
+++ vk_data_paser.user.js
@@ -6,6 +6,8 @@
 // @author       You
 // @match        https://vk.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=vk.com
+// @updateUrl    https://github.com/DeveloperOfEverything/JSScripts/raw/master/vk_data_paser.user.js
+// @downloadUrl  https://github.com/DeveloperOfEverything/JSScripts/raw/master/vk_data_paser.user.js
 // @grant        none
 // ==/UserScript==
 
