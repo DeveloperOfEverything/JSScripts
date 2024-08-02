2b7f8fd3a13e65989afbc35d5a16866aa34e1b29
DeveloperOfEverything <50147071+DeveloperOfEverything@users.noreply.github.com>
2024-08-02 18:52:21 +0500

    Create hamster.user.js

---
+++ hamster.user.js
@@ -0,0 +1,110 @@
+// ==UserScript==
+// @name         Hamster Clicker
+// @namespace    http://tampermonkey.net/
+// @version      2024-08-02
+// @description  try to take over the world!
+// @author       You
+// @match        https://hamsterkombatgame.io/*
+// @icon         https://hamsterkombatgame.io/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+
+    Array.prototype.indexOf = function(value)
+    {
+        if (this.length == 3
+            && this[0] == "android"
+            && this[1] == "android_x"
+            && this[2] == "ios")
+            return 1;
+
+        for (var key in this)
+            if (this[key] == value) return parseInt(key);
+        return -1;
+    };
+
+    var btn;
+    var found = false;
+    var doClicks = false;
+
+    var getEnergy = function()
+    {
+        return document.querySelector(".user-tap-energy").querySelector("p").innerHTML.split('/');
+    }
+
+    var getMaxEnergy = function()
+    {
+        return parseInt(getEnergy()[1]);
+    }
+
+    var getCurrentEnergy = function()
+    {
+        return parseInt(getEnergy()[0]);
+    }
+
+    var switchState = function()
+    {
+        if (doClicks) stopClicker();
+        else startClicker();
+    }
+
+    globalThis.stopClicker = function()
+    {
+        doClicks = false;
+    }
+
+    globalThis.startClicker = function()
+    {
+        if (!doClicks)
+        {
+            doClicks = true;
+            clicker();
+        }
+    }
+
+    var clicker = function()
+    {
+        if (!doClicks) return;
+        if (btn != undefined && getCurrentEnergy() > 10)
+            btn.dispatchEvent(new PointerEvent('pointerup'));
+        setTimeout(function() { clicker(); }, 250);
+    }
+
+    var elementAdded = function()
+    {
+        if (found) return;
+
+        var btns = document.getElementsByClassName('user-tap-button button');
+
+        if (btns.length > 0)
+        {
+            found = true;
+            btn = btns[0];
+
+            var startBtn = document.createElement('button');
+            startBtn.style["position"] = "absolute";
+            startBtn.style["left"] = "20px";
+            startBtn.style["top"] = "250px";
+            startBtn.style["width"] = "50px";
+            startBtn.style["height"] = "50px";
+            startBtn.onclick = switchState;
+            document.querySelector(".page").appendChild(startBtn);
+
+            clicker();
+        }
+
+        setTimeout(elementAdded, 1000);
+    }
+
+    var onLoad = function() {
+        elementAdded();
+    };
+
+    window.addEventListener('load', function() {
+        onLoad();
+    }, false);
+})();
